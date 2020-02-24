1257b28a813edc11ed018ed0926dd309981379f6
Lucas <lucasbezerra1701@gmail.com>
2020-02-24 18:55:43 -0300

    CAP02#ITEM2.6

---
+++ server/config/express.js
@@ -0,0 +1,25 @@
+/* Código simplório, apenas para fornecer o serviço para a aplicação */
+
+const express = require('express');
+const app = express();
+const routes = require('../app/routes');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+app.set('clientPath', path.join(__dirname, '../..', 'client'));
+console.log(app.get('clientPath'));
+app.use(express.static(app.get('clientPath')));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
+routes(app);
+
+module.exports = app;
